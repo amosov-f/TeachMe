a14b27134ad1dd8e70ebf064e9c26a128e850e6a
afaf <amosov.f@mail.ru>
2013-07-16 02:29:28 +0400

    Added caching of data in StatusDepot

--- teachme/src/main/java/com/kk/teachme/db/StatusDepot.java
+++ teachme/src/main/java/com/kk/teachme/db/StatusDepot.java
@@ -9,10 +9,47 @@ import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatusDepot {
 
     SimpleJdbcTemplate jdbcTemplate;
+
+    private Map<Integer, Status> id2status = new HashMap<Integer, Status>();
+
+    public void init() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        List<Map.Entry<Integer, Status>> statuses = jdbcTemplate.query(
+                                "select * from problem_status",
+                                getRowMapper()
+                        );
+                        Map<Integer, Status> id2status = new HashMap<Integer, Status>();
+                        for (Map.Entry<Integer, Status> entry : statuses) {
+                            id2status.put(entry.getKey(), entry.getValue());
+                        }
+                        StatusDepot.this.id2status = id2status;
+
+                        System.out.println("Loaded");
+                    } catch (Throwable tr) {
+                        tr.printStackTrace();
+                    } finally {
+                        try {
+                            Thread.sleep(60000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
 
     public boolean setStatus(User user, Problem problem, Status status) {
         jdbcTemplate.update(
@@ -32,35 +69,25 @@ public class StatusDepot {
                 problem.getId()
         );
 
-        Status result = jdbcTemplate.query(
-                "select status from problem_status where id = ?",
-                getStatusRowMapper(),
-                status_id
-        ).get(0);
-
-        return result == null ? Status.NEW : result;
+        return id2status.get(status_id);
     }
 
     public int getStatusId(Status status) {
-        return jdbcTemplate.query(
-                "select id from problem_status where status = ?",
-                getIdRowMapper(),
-                status.toString().toLowerCase()
-        ).get(0);
-    }
-
-    private ParameterizedRowMapper<Status> getStatusRowMapper() {
-        return new ParameterizedRowMapper<Status>() {
-            public Status mapRow(ResultSet resultSet, int i) throws SQLException {
-                return Status.valueOf(resultSet.getString("status").toUpperCase());
+        for (Map.Entry<Integer, Status> entry : id2status.entrySet()) {
+            if (entry.getValue().equals(status)) {
+                return entry.getKey();
             }
-        };
+        }
+        return -1;
     }
 
-    private ParameterizedRowMapper<Integer> getIdRowMapper() {
-        return new ParameterizedRowMapper<Integer>() {
-            public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
-                return resultSet.getInt("id");
+    private ParameterizedRowMapper<Map.Entry<Integer, Status>> getRowMapper() {
+        return new ParameterizedRowMapper<Map.Entry<Integer, Status>>() {
+            public Map.Entry<Integer, Status> mapRow(ResultSet resultSet, int i) throws SQLException {
+                return new AbstractMap.SimpleEntry(
+                        resultSet.getInt("id"),
+                        Status.valueOf(resultSet.getString("status").toUpperCase())
+                );
             }
         };
     }
