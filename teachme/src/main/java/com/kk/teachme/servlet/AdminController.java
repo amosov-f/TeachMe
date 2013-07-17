251447d21f95c53fcad9ed5db2de312c58419432
dkachmar <dkachmar@yandex.ru>
2013-07-17 21:14:32 +0400

    empty tag bugfixing

--- teachme/src/main/java/com/kk/teachme/servlet/AdminController.java
+++ teachme/src/main/java/com/kk/teachme/servlet/AdminController.java
@@ -3,18 +3,13 @@ package com.kk.teachme.servlet;
 import com.kk.teachme.db.*;
 import com.kk.teachme.model.Problem;
 import com.kk.teachme.model.Tag;
-import com.kk.teachme.model.User;
-import com.kk.teachme.support.JSONCreator;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -41,7 +36,7 @@ public class AdminController {
             Model model,
             @RequestParam String name,
             @RequestParam String statement,
-            @RequestParam String tags,
+            @RequestParam(required = false) String tags,
             @RequestParam String solution,
             @RequestParam int checker_id
     ) {
@@ -50,8 +45,10 @@ public class AdminController {
         solution = solution.trim();
 
         List<Tag> tagList = new ArrayList<Tag>();
-        for (String tagName : tags.replace('_', ' ').split(",")) {
-            tagList.add(tagDepot.getByName(tagName));
+        if (tags != null) {
+            for (String tagName : tags.replace('_', ' ').split(",")) {
+                tagList.add(tagDepot.getByName(tagName));
+            }
         }
 
         Problem newProblem = new Problem(name, statement);
