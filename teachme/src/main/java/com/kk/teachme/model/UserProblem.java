c7e33edd6084b2771a9d6e3b20009758da80557b
kharkovsky <kharkovsky.spb@mail.ru>
2013-07-21 22:46:01 +0300

    fixed UserProblemDepot

--- teachme/src/main/java/com/kk/teachme/model/UserProblem.java
+++ teachme/src/main/java/com/kk/teachme/model/UserProblem.java
@@ -3,12 +3,10 @@ package com.kk.teachme.model;
 
 public class UserProblem {
     private Problem problem;
-    private int userId;
     private Status status;
 
-    public UserProblem(Problem problem, int userId, Status status) {
+    public UserProblem(Problem problem, Status status) {
         this.problem = problem;
-        this.userId = userId;
         this.status = status;
     }
 
@@ -16,20 +14,12 @@ public class UserProblem {
         return problem;
     }
 
-    public int getUser_id() {
-        return userId;
-    }
-
     public Status getStatus() {
         return status;
     }
 
-    public void setId(Problem problem) {
+    public void setProblem(Problem problem) {
         this.problem = problem;
-    }
-
-    public void setUser_id(int userId) {
-        this.userId = userId;
     }
 
     public void setStatus(Status status) {
