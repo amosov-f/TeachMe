30f6a643f1136aee198d1aff63ecd30468832f6b
Mark <markyezhkov@yandex.ru>
2013-08-10 16:38:39 +0400

    Exceptions in TCTLoader handled better. Search by List<Tag> in ProblemDepot added.

--- teachme/src/main/java/com/kk/teachme/model/Tag.java
+++ teachme/src/main/java/com/kk/teachme/model/Tag.java
@@ -31,4 +31,21 @@ public class Tag {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        if (name != null ? !name.equals(tag.name) : tag.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
