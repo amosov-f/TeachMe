d06da656659610e98698a747adfedaf73ae8b5c1
Mark <markyezhkov@yandex.ru>
2013-08-13 15:12:13 +0400

    UserProblem handling altered

--- teachme/src/main/java/com/kk/teachme/db/UserProblemDepot.java
+++ teachme/src/main/java/com/kk/teachme/db/UserProblemDepot.java
@@ -111,6 +111,59 @@ public class UserProblemDepot {
 
     }
 
+
+    public List<UserProblem> getProblemsByTagIdList(int userId, List<Integer> tagIdList) {
+
+        List<Tag> tagList = new ArrayList<Tag>();
+        for (Integer tagId : tagIdList) {
+            tagList.add(tagDepot.getById(tagId));
+        }
+        return getProblemsByTagList(userId, tagList);
+
+    }
+
+    public List<UserProblem> getProblemsByTagList(int userId, List<Tag> tagList) {
+
+        if (tagList == null || tagList.isEmpty()) {
+            return new ArrayList<UserProblem>();
+        }
+
+        List<Problem> problemList = problemDepot.getByTagList(tagList);
+
+        List<UserProblem> userProblems = jdbcTemplate.query("select up.problem_id, up.status_id " +
+                "from user_problem up inner join problem_tag pt on pt.problem_id = up.problem_id " +
+                "where up.user_id = ? and pt.tag_id = ?",
+                getRowMapper(),
+                userId,
+                tagList.get(0).getId());
+
+        List<UserProblem> userProblemList = new ArrayList<UserProblem>();
+        for (UserProblem userProblem : userProblems) {
+            if (userProblem.getProblem().getTags().containsAll(tagList)) {
+                userProblemList.add(userProblem);
+            }
+        }
+
+        List<UserProblem> resultUserProblemList = new ArrayList<UserProblem>();
+
+        for (Problem problem : problemList) {
+
+            UserProblem newUserProblem = new UserProblem(problem);
+            resultUserProblemList.add(newUserProblem);
+
+            for (UserProblem userProblem : userProblems) {
+                if (userProblem.getProblem().getId() == newUserProblem.getProblem().getId()) {
+                    newUserProblem.setStatus(userProblem.getStatus());
+                    break;
+                }
+            }
+
+        }
+
+        return resultUserProblemList;
+
+    }
+
     protected ParameterizedRowMapper<UserProblem> getRowMapper() {
         return new ParameterizedRowMapper<UserProblem>() {
             public UserProblem mapRow(ResultSet resultSet, int i) throws SQLException {
